b30a1738d888623fea04e4a006a60ed25e980bcc
ADEDEJI <emmanueladedeji222@gmail.com>
2025-10-08 02:29:14 +0100

    initial project setup with Vite and Tailwind

---
+++ elevana-africa/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App(){
+ 
+
+  return (
+    
+      <div className="text-center mt-20">
+           <h1 className="text-[50px]">Elevana Africa</h1>
+          <p className='text-blue-500'>Tailwind Active</p>
+      </div>
+      
+    
+  );
+}
+
+export default App;
