afef996eb2c1bbcc6ccab8d38befc8e38f7a12dc
ADEDEJI <emmanueladedeji222@gmail.com>
2025-10-19 00:59:40 +0100

    error changes

--- elevana-africa/src/pages/index.jsx
+++ elevana-africa/src/pages/index.jsx
@@ -1,14 +1,12 @@
-import Navigation from "../components/navigation"
-import Hero from "../components/hero"; 
-import About from "../components/about";
-import Features from "../components/features";    
-import HowItWorks from "../components/howitworks";
-import Testimonials from "../components/testimonials";
-import CTA from "../components/CTA";
+import Navigation from "@/components/navigation";
+import Hero from "@/components/hero";
+import About from "@/components/about";
+import Features from "@/components/features";
+import HowItWorks from "@/components/howitworks";
+import Testimonials from "@/components/testimonials";
+import CTA from "@/components/CTA";
 
 import Footer from "../components/footer";
-
-
 
 const Index = () => {
   return (
@@ -21,7 +19,6 @@ const Index = () => {
       <Testimonials />
       <CTA />
       <Footer />
-        
     </div>
   );
 };
