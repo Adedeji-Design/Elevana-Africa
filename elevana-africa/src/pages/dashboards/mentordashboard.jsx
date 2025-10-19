b8f4ae1f7652712ea8bb58e0c72b6bc621c8ee9c
ADEDEJI <emmanueladedeji222@gmail.com>
2025-10-19 01:13:26 +0100

    error in naming fixed

--- elevana-africa/src/pages/dashboards/mentordashboard.jsx
+++ elevana-africa/src/pages/dashboards/mentordashboard.jsx
@@ -1,4 +1,4 @@
-import { DashboardLayout } from "@/components/dashboard/DashboardLayout";
+import { DashboardLayout } from "@/components/dashboard/dashboardlayout";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import   Button  from "@/components/button";
 import { Progress } from "@/components/ui/progress";
